84c8cf17147e573f466c1aa9b6f7023b6a2f5c84
kayeongkim1 <kykim097@gmail.com>
2022-03-18 17:44:39 +0900

    todolists

---
+++ app.js
@@ -0,0 +1,35 @@
+// const loginForm = document.querySelector("#login-form");
+// const loginInput = document.querySelector("#login-form input");
+// //const loginButton = document.querySelector("#login-form button");
+// const greeting = document.querySelector("#greeting");
+
+// const USERNAME_KEY = "username";
+
+// //function onLoginBtnClick() {
+//     const HIDDEN_CLASSNAME = "hidden";
+//     //const username = loginInput.value;
+//     //console.log(username);
+// //}
+
+
+// function onLoginSubmit(event) {
+//     event.preventDefault();
+//     loginForm.classList.add(HIDDEN_CLASSNAME);
+//     const username = loginInput.value;
+//     localStorage.setItem("USERNAME_KEY", username);
+//     paintGreetings(username);
+// }
+
+// function paintGreetings(username) {
+//     greeting.innerText = `Hello ${username}`;
+//     greeting.classList.remove(HIDDEN_CLASSNAME);
+// }
+
+// const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+// if (savedUsername === null) {
+//     loginForm.classList.remove(HIDDEN_CLASSNAME);
+//     loginForm.addEventListener("submit", onLoginSubmit);
+// } else {
+//     paintGreetings(savedUsername);
+// }
